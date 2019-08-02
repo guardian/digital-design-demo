f3d0e5272824fae4ab1a436c8bd54092bf7fe99c
Zef Cherry-Kynaston <zef_cherry-kynaston@35054.gnm.int>
2019-08-02 15:53:24 +0100

    ZEF RULEZ

---
+++ main.js
@@ -0,0 +1,12 @@
+// Seal donation message
+// alert('We urgently need more seals.');
+
+console.log("If you are reading this, we urgently need more seals");
+
+document.querySelector('header-title');
+
+var title = document.querySelector('.header-title');
+
+setTimeout(function() {
+    title.classList.add('approved');
+}, 1000)
